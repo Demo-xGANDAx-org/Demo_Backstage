313fb8e757ff42591d7d27d863dbfebc24f76666
xGANDAx <jonathanchitivam@gmail.com>
2026-04-19 17:14:24 -0500

    Add identity provider

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,6 +2,31 @@ import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
 
+import { googleAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+    (
+      <SignInPage
+        {...props}
+        provider={{
+          id: 'github-auth-provider',
+          title: 'Google',
+          message: 'Sign in using Google',
+          apiRef: googleAuthApiRef,
+        }}
+      />
+    ),
+  },
+});
+
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, navModule, createFrontendModule({
+    pluginId: 'app',
+    extensions: [signInPage],
+  }),],
 });
